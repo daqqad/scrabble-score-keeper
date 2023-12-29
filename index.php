d43cc38f003c389dbc43199a2949dd4349550f00
Alex Olshansky <alex@daqfx.com>
2023-12-29 00:09:45 -0800

    First commit with basic skeleton.

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="public/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="public/bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
+
